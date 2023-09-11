6463ebfd5dbae4fa63b1f5c6fe0ca69a904d90df
FerOrdSom <fordsom@gmail.com>
2023-09-11 19:13:49 +0200

    single page simple routing

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-window.addEventListener("load", ()=>{
-    var content = document.getElementById("content");
-
+const routes = {
+    "about-nav" : "./pages/about-me.html",
+    "projects-nav" : "./pages/projects.html",
+    "blog-nav": "./pages/blog.html",
+}
+console.log(routes["projects-nav"]);
+window.addEventListener("load", () => {
+    var page = "./pages/about-me.html"; //this is part of the state of the page
+    injectContent(page); //when state changes this runs loads content
+    var navButtons = document.getElementsByClassName("nav-buttons");
+    
+    for (let i = 0; i < navButtons.length; i++){
+        var contentElement = document.getElementById("content");
+        navButtons[i].addEventListener("click", ()=>{
+            let destination = navButtons[i].id;
+            page = routes[destination];
+            console.log(routes[destination]);
+            contentElement.innerHTML = "";
+            injectContent(page);
+        })
+        
+    }
+        
+    
     // var state = {language : "es"};
     // var langFlag = document.getElementById("flag");
     // var langSelector = document.getElementById("lang");
@@ -15,35 +36,27 @@ window.addEventListener("load", ()=>{
     //     }
     // })
     // console.log(state.language);
-})
+});
 
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
+function injectContent(page) {
+    var elmnt = document.getElementById("content");
+    var xhttp = new XMLHttpRequest();
+    //WTF it doesn't work if I use ()=>{...}????
+    //ANSWER: Arrow functions don't have their own bindings to this, arguments, or super, and should not be used as methods.
+    //with ()=>: this: [object Window]
+    //with function (): this: [object XMLHttpRequest]
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) { // 4 = operation complete            
+            if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+            if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
         }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
-      }
     }
-  }
+    xhttp.open("GET", page, true);
+    xhttp.send();
+    return;
+}
+
+
 
 
 
